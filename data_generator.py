68696f9e73ece8bd10431e90ecedc2dcfa069b54
zumbie138 <gabriel.mks@gmail.com>
2026-07-09 19:16:53 -0300

    atualizado a mudança de dia ao passar da meia noite

--- data_generator.py
+++ data_generator.py
@@ -48,6 +48,10 @@ class DataGenerator:
         elif isinstance(hora_calculada, datetime):
             return (hora_calculada + timedelta(minutes=minutos, seconds=segundos))
     
+    def _verificar_data(self, hora, hora_anterior):
+        if hora.time() < hora_anterior.time():
+            self.data = (datetime.strptime(self.data, '%d/%m/%Y') + timedelta(days=1)).strftime('%d/%m/%Y')
+    
     def _adicionar_dados(self, evento, camara, data, hora, usuario, trilho, valvula, temperatura, pressao):
         self.dados.append({
             'Evento': evento,
@@ -63,6 +67,7 @@ class DataGenerator:
         
     def ciclo_inicial(self):
         hora_ligamento = self._calcular_hora(self.hora, 0, 39)
+        hora_anterior = hora_ligamento
         self._adicionar_dados(
            f'CARREGOU A {self.receita} NA {self.camara}',
            self.camara, self.data, hora_ligamento.time(),
@@ -73,36 +78,43 @@ class DataGenerator:
             valv = valvulas[1] + i
             segundo_acionameto = random.randint(11, 50)
             hora_ligamento = self._calcular_hora(hora_ligamento, 0, segundo_acionameto)
+            self._verificar_data(hora_ligamento, hora_anterior)
             self._adicionar_dados(
                 f'HABILITOU PROCESSO: {self.camara} - VALV{valv}, {self.receita}',
                 self.camara, self.data, hora_ligamento.time(), 'gq',
                 f'Trilho {i+1}', f'V{valv}', None, None
             )
+            hora_anterior = hora_ligamento
         segundo_acionameto = random.randint(11, 50)
         self.hora_ciclo = self._calcular_hora(hora_ligamento, 0, segundo_acionameto)
     
     def ciclo_principal(self):
         ciclo = round(self.receita_minutos[self.receita] / 3)
+        hora_anterior = self.hora_ciclo
         for i in range(int(ciclo)):
             trilho, valvula, segundo_acionamento, temperatura, pressao = self._gerar_dados_aleatorios()
             hora = self._calcular_hora(self.hora_ciclo, 0, segundo_acionamento)
             self.hora_ciclo = self._calcular_hora(self.hora_ciclo, 3, 0)
+            self._verificar_data(hora, hora_anterior)
             self._adicionar_dados(
                 f'TEMPERATURA = {temperatura:.1f}°C, PRESSÃO = {pressao:.1f} bar',
                 self.camara, self.data, hora.time(), 'SISTEMA', f'Trilho {trilho}',
                 f'V{valvula}', f'{temperatura:.2f}', f'{pressao:.2f}'
             )
+            hora_anterior = hora
     
     def ciclo_encerramento(self):
         valvulas = self.camara_valvulas[self.camara]
         hora = self.hora_ciclo
+        hora_anterior = hora
         for i in range(valvulas[0]):
             _, valvula, segundo_acionamento, *_ = self._gerar_dados_aleatorios()
             valv_num = valvulas[1] + i
-            hora = self._calcular_hora(hora, 0, segundo_acionamento) 
+            hora = self._calcular_hora(hora, 0, segundo_acionamento)
+            self._verificar_data(hora, hora_anterior)
             self._adicionar_dados(
                 f'DESABILITOU PROCESSO: {self.camara}- VALV{valv_num},  {self.receita}',
                 self.camara, self.data, hora.time(), 'SISTEMA', f'Trilho {i+1}',
                 f'V{valvula}', None, None
             )
-            
+            hora_anterior = hora
